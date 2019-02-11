9dee47b59af8780fa780e0b4caa85ba544974df1
Karina Carmona <kgrjng@gmail.com>
2019-02-11 15:45:54 -0500

    Add modal!

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
   <div id="app">
     <Board/>
+    <Modal v-bind:modalAction="modalAction" v-show="showModal"/>
   </div>
 </template>
 
 <script>
 import Board from './components/Board.vue';
+import Modal from './components/Modal.vue';
+import { mapState } from 'vuex';
 
 export default {
   name: 'app',
+  data () {
+    return{
+    }
+  },
   components: {
     Board,
+    Modal,
   },
+  computed: mapState([
+      'showModal',
+      'modalAction'
+    ]),
+  methods: {
+  }
 };
 </script>
 
 <style lang="scss">
+@import "./assets/css/resetr.css";
+
 body {
   background-color: #F4F3F8;
-  font-family: "Trebuchet MS", Helvetica, sans-serif
+  font-family: "Trebuchet MS", Helvetica, sans-serif;
+  width: 100%;
+  height:100%;
+}
+
+#app{
+  display: inline-block;
+  width: 100%;
 }
 </style>

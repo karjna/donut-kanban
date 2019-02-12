a69684bf96e10151e7dfabfdd0213b9325c5c586
Karina Carmona <kgrjng@gmail.com>
2019-02-11 22:17:33 -0500

    Add modal template

---
+++ src/components/Modal.vue
@@ -0,0 +1,145 @@
+<template>
+  <!-- remove modalaction and then maybe do it by count of todo objects you're editing -->
+  <div class="modal">
+    <div class = "modal__body">
+      <div class = "modal__content">
+        <div class = "modal__header" v-if="showEdit">Edit Task</div>
+        <div class = "modal__header" v-else>Create New Task</div>
+        <form>
+          <input type="text" placeholder="Title"/>
+          <textarea placeholder="Description (optional)"></textarea>
+          <input type="date" placeholder="Due On"/>
+        </form>
+        <div class = "modal__footer">
+          <div class = "btn btn--red" v-if="showEdit" @click="">Delete</div>
+          <div class = "float-right">
+            <div class = "btn btn--white" @click="hideModal">Cancel</div>
+            <div class = "btn btn--purple" @click="" v-if="showEdit">Save</div>
+            <div class = "btn btn--purple" @click="" v-if="!showEdit">Create</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class = "modal__bg" @click="hideModal"></div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Modal',
+  props: {
+    modalAction: String,
+  },
+  computed: {
+    showEdit: function() {
+      return this.modalAction == 'edit'
+    }
+  },
+  methods:{
+    hideModal: function(){
+      this.$store.commit('hideModal');
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .modal{
+    width:100%;
+    height:100%;
+  }
+  .modal__bg{
+    background-color: rgba(255, 255, 255, 0.5);
+    width: 100%;
+    height:100%;
+    position: absolute;
+    top:0;
+    left:0;
+    z-index: 2;
+  }
+
+  .modal__body{
+    background-color: #fff;
+    z-index: 3;
+    position: absolute;
+    top:50px;
+    left:0;
+    right:0;
+    margin: auto;
+    max-width: 420px;
+    border-radius: 10px;
+    box-shadow: 1px 2px 20px 2px #d0c8d8;
+    padding: 30px;
+  }
+
+  .modal__header{
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+
+  input, textarea{
+    font-size: 12px;
+    border-radius: 4px;
+    border: 2px solid #E9E9F0;
+    width: 100%;
+    margin-bottom:10px;
+    padding: 7px 15px;
+    -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+     box-sizing: border-box;
+    &::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+      color: #908B99;
+      font-style: italic;
+    }
+    &::-moz-placeholder { /* Firefox 19+ */
+      color: #908B99;
+      font-style: italic;
+    }
+    &:-ms-input-placeholder { /* IE 10+ */
+      color: #908B99;
+      font-style: italic;
+    }
+    &:-moz-placeholder { /* Firefox 18- */
+      color: #908B99;
+      font-style: italic;
+    }
+  }
+
+  input{
+    height:30px;
+  }
+
+  textarea {
+    min-height: 60px;
+  }
+
+  .btn{
+    display: inline-block;
+    padding: 10px 15px;
+    font-size: 13px;
+    color: white;
+    border-radius: 5px;
+  }
+
+  .btn--purple{
+    background-color: #7658F6;
+    &:hover{
+      background-color: #5132d4;
+    }
+  }
+
+  .btn--white{
+    color: #7658F6;
+  }
+
+  .btn--red{
+    background-color: #EB3430;
+  }
+
+  .float-right{
+    float: right;
+  }
+</style>
